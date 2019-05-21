eb4cb6b15e8ac3c20dc76bc55d52453e0140c664
fastiz <facuastiz@gmail.com>
2019-05-21 18:38:32 -0300

    Configuracion inicial.

---
+++ bin/include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by fastiz on 21/05/19.
+//
+
+#ifndef PROXYSERVERHTTP1_1_MAIN_H
+#define PROXYSERVERHTTP1_1_MAIN_H
+
+#endif //PROXYSERVERHTTP1_1_MAIN_H
---
+++ bin/main.c
@@ -0,0 +1,10 @@
+//
+// Created by fastiz on 21/05/19.
+//
+
+#include "include/main.h"
+
+
+int main(int arg){
+    int a = 0;
+}
